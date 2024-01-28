872df30a731137a823ac81bd576b28584c2c61af
KEEMO-DEV <zaxgamert@gmail.com>
2024-01-29 00:30:06 +0700

    New Design

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 async function getPlayerLength(ip) {
     if (!ip || typeof ip !== "string")
         throw new Error(`ip and port must be present and must be a string`);
-    const data = await fetch(`https://${ip}/players.json`);
+    const data = await fetch(`http://${ip}/players.json`);
     const json = await data.json();
     return json.length;
+}
+async function getMaxPlayers(ip) {
+    if (!ip || typeof ip !== "string")
+        throw new Error(`ip and port must be present and must be a string`);
+    const data = await fetch(`http://${ip}/info.json`);
+    const json = await data.json();
+    return json.vars.sv_maxClients;
 }
 
 function discord_message(webHookURL, message) {
@@ -21,11 +28,13 @@ var LowPlayer = false
 setInterval(async () => {
     const ip = "15.204.216.10:30120"
     var playercount = await getPlayerLength(ip);
+    var maxplayer = await getMaxPlayers(ip);
     $('#now_player').text(playercount);
+    $('#max_player').text(maxplayer);
     if (playercount <= 100 && LowPlayer == false) {
         LowPlayer = true
         discord_message('https://discord.com/api/webhooks/936650461090549810/VWceX2NluZTVmNV6h1NkfMI5MkCfEXICyO6el0mWvVsjrRp8IarVThfC8i0ePTTulM29', `ตอนนี้มีคนน้อยกว่า 100 คนแล้วนะครับ ${playercount}/220`)
     } else if (playercount > 100 && LowPlayer == true) {
         LowPlayer = false
     }
-}, 500);
+}, 100);
